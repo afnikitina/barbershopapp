7bc8c769741558ba94d5a3792d3160c9962e612a
Asya Nikitina <anikitina@cnm.edu>
2018-10-15 01:27:53 -0600

    Modified the Barber model.

--- app/Barber.php
+++ app/Barber.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barber extends Model
 {
-    //
+    protected $fillable = [
+    	 'name',
+		 'address',
+		 'email',
+		 'phone',
+		 'ast'
+	 ];
 }
