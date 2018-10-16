3c54757cefb51a5ef20d7f11118726f1718745bc
Asya Nikitina <anikitina@cnm.edu>
2018-10-15 19:25:57 -0600

    Updated the welcome view.

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Welcome to Our Barber Shop</title>
-    </head>
-    <body>
-        <h1>Welcome to Our Barber Shop!</h1>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to Our Barber Shop!</h1>
+@endsection
