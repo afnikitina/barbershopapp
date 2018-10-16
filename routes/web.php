48d61bb92afaec603cb50996901af545b34b5d9c
Asya Nikitina <anikitina@cnm.edu>
2018-10-15 19:30:50 -0600

    Created the QueueController and the corresponding view.

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,7 @@ Route::get('/', 'WelcomeController@index')->name('welcome.index');
 Route::get('barbers', 'BarbersController@index')->name('barbers.index');
 Route::get('barbers/{id}', 'BarbersController@show')->name('barbers.show');
 
+Rout::get('queue', 'QueueController@index')->name('queue.index');
+
 Route::view('about', 'about.index')->name('about.index');
 Route::view('contact', 'contact.index')->name('contact.index');
