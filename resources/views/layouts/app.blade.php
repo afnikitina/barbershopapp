7fcc8462a4149a587128dc50f3502de64400ee02
Asya Nikitina <anikitina@cnm.edu>
2018-10-15 19:25:31 -0600

    Created the master layout.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Welcome to Our Barber Shop</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
