0b3e26406fb8c6895b902fe1194236106d88deb9
Asya Nikitina <anikitina@cnm.edu>
2018-10-17 01:52:59 -0600

    Updated the 'edit profile' view.

--- resources/views/barbers/edit.blade.php
+++ resources/views/barbers/edit.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Barber')
-
 @section('content')
-    <h1>Edit Profile: {!! $barber->name !!}</h1>
+    <div class="create-barber-form" style="font-size:1.2em;font-weight:bolder;">
+        <div class="row justify-content-md-center">
+            <h1>Edit Profile: {!! $barber->name !!}</h1>
+        </div>
+        <div class="form-grop">
+            {!! Form::model(['method' => 'PATCH', 'action' => ['BarbersController@update', $barber->id], 'class' => 'form']) !!}
+            @include('barbers._form', ['submitButtonText' => 'Update Profile'])
+            {!! Form::close() !!}
+            <div class="row justify-content-md-center">
+                <div class="form-group col-md-6">
+                    @include('errors.list')
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
