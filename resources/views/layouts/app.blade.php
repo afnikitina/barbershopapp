d5d34a12bd9feb4eeff94a607a6a9ebb7dd4bd72
Asya Nikitina <anikitina@cnm.edu>
2018-10-18 16:45:45 -0600

    Modified the title in the master layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Bob's Barber Shop | @yield('title', 'Home Page') </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
