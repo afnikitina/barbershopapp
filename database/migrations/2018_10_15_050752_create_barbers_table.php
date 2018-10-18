28f4cf4674a25ffa5b777e1936b6697d0b44c8ad
Asya Nikitina <anikitina@cnm.edu>
2018-10-18 01:09:20 -0600

    Updated the barbers and users tables (the column names and added a foreign key).

--- database/migrations/2018_10_15_050752_create_barbers_table.php
+++ database/migrations/2018_10_15_050752_create_barbers_table.php
@@ -15,18 +15,18 @@ class CreateBarbersTable extends Migration
     {
         Schema::create('barbers', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('user_id')->unsigned()->nullable();
 			  	$table->string('name');
 			  	$table->string('address');
 			  	$table->string('email')->unique();
 			  	$table->string('Phone');
-			  	$table->tinyInteger('ast')->unsigned()->default(30);
 			   $table->timestamps();
 
 			   $table->foreign('user_id')
 					->references('id')
 					->on('users')
 					->onDelete('cascade');
+
         });
     }
 
@@ -37,6 +37,7 @@ class CreateBarbersTable extends Migration
      */
     public function down()
     {
+		 //$table->dropForeign('barbers_user_id_foreign');
         Schema::dropIfExists('barbers');
     }
 }
