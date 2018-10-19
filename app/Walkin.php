e9876d20ecae2b4bf5a147976ea6697e6c2183fe
Asya Nikitina <anikitina@cnm.edu>
2018-10-18 18:37:32 -0600

    Added a setter to the Walkin model.

--- app/Walkin.php
+++ app/Walkin.php
@@ -8,5 +8,22 @@ class Walkin extends Model
 {
     protected $fillable = [
 		 'name'
-	 ]
+	 ];
+
+	/**
+	 * We are going to save the service time into the database
+	 * @param $value (selected value of the dropdown list)
+	 */
+	public function setServiceTimeAttribute($value) {
+    	$services = [
+			'tr_cut' => 20,
+			'sp_cut' => 25,
+			'beard' => 10,
+			'shave' => 15,
+			'cut_beard' => 30,
+			'cut_shave' => 35
+		];
+
+    	$this->attributes['service_time'] = $services[$value];
+	 }
 }
