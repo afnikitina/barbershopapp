301ce231ed9aa3f58e298c051bf00256b572d365
Asya Nikitina <anikitina@cnm.edu>
2018-10-19 02:14:07 -0600

    Started writing business logic.

--- app/Http/Controllers/WalkinController.php
+++ app/Http/Controllers/WalkinController.php
@@ -20,6 +20,20 @@ class WalkinController extends Controller
 		return view('walkins.create');
 	}
 
+
+
+	// modify!!!
+	function findMinAndReplace(array $arr1, array $arr2) {
+		$min = min($arr1);
+		$index = array_search($min, $arr1);
+		$arr1[$index] += $arr2[$index];
+		return $arr1;
+	}
+
+
+
+
+
 	public function calculateWaitingTime() {
 		// EOB (7 pm)
 		$eobTime = Carbon::createFromTime(19, 0, 0, 'America/Denver');
@@ -36,7 +50,20 @@ class WalkinController extends Controller
 		}
 
 		$timeArr = [];
-		foreach ($barbers as $barber) array_push($timeArr, $barber->updated_at);
+		foreach ($barbers as $barber)
+			array_push($timeArr, $barber->updated_at);
+
+		// transform the timestamps into the number of minutes between service start time and now
+		foreach ($timeArr as &$currTms) $currTms = $currTime->diffInMinutes($currTms);
+
+		// get the array of 'walkins' service times
+		$queue = DB::table('workins')->oldest('created_at')->get();
+		$st = [];
+			for ($i = 0; $i < count($queue); $i++ ) {
+				$st[$i] = $queue[$i]['service_time'];
+			}
+
+
 
 
 
