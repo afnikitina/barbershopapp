7c482b9cf26600124c8d3d7922f474f01215eb16
Asya Nikitina <anikitina@cnm.edu>
2018-10-20 22:14:22 -0600

    Created a function for calculating the waiting time in line.

--- app/Http/Controllers/WalkinController.php
+++ app/Http/Controllers/WalkinController.php
@@ -5,10 +5,18 @@ use App\Http\Requests\AddCustomerRequest;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
+use App\Barber;
 use App\Walkin;
 use App\Worklog;
 
 use Illuminate\Http\Request;
+
+public function findMinAndReplace(&$arr, $num) {
+	$min = min($arr);
+	$index = array_search($min, $arr);
+	$arr[$index] += $num;
+	return $min;
+}
 
 class WalkinController extends Controller
 {
@@ -28,48 +36,43 @@ class WalkinController extends Controller
 	 * @param $num
 	 * @return mixed
 	 */
-	function findMinandReplace(&$arr, $num) {
-		$min = min($arr);
-		$index = array_search($min, $arr);
-		$arr[$index] += $num;
-		return $min;
 	}
 
-	public function calculateWaitingTime() {
-		// EOB (7 pm)
-		$eobTime = Carbon::createFromTime(19, 0, 0, 'America/Denver');
+	protected $sobTime = Carbon::createFromTime(7, 0, 0, 'America/Denver');
+	protected $eobTime = Carbon::createFromTime(19, 0, 0, 'America/Denver');
 
+	public function calculateWaitingTime() {
 		// current time
 		$currTime = Carbon::now();
 
-		// find out how many barbers are currently working
-		$barbers = DB::table('worklogs')->get();
+		// get all records from worklog
+		$workstations = Worklog::all();
 
 		// if none barbers are working, return -1 and exit from the function, waiting time is infinity
-		if (!$barbers) {
+		if (!$workstations->count()) {
 			return -1;
 		}
 
 		$timeArr = [];
-		foreach ($barbers as $barber)
-			array_push($timeArr, $barber->updated_at);
+		foreach ($workstations as $workstation)
+			array_push($timeArr, $workstation->updated_at);
 
 		// transform the timestamps into the number of minutes between service start time and now
 		foreach ($timeArr as &$currTms) $currTms = $currTime->diffInMinutes($currTms);
 
 		// get the array of 'walkins' service times
-		$queue = DB::table('workins')->oldest('created_at')->get();
+		$queue = Walkin::all()->orderBy('created_at', 'DESC')->get();
 		$st = [];
 		for ($i = 0; $i < count($queue); $i++ ) {
 			$st[$i] = $queue[$i]['service_time'];
 		}
 
 		// calculate the current waiting time
-
-
-
-
-
+		$waitingTime = 0;
+		for ($i = 0; $i < count($queue); $i++ ) {
+			$waitingTime = findMinAndReplace($st, $waitingTime);
+		}
+		return $waitingTime;
 	}
 
 	/**
