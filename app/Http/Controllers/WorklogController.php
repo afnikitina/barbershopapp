5bcd67e9a4416a9c28190f1d1df41e9ec66a8f08
Asya Nikitina <anikitina@cnm.edu>
2018-10-21 21:30:14 -0600

    Made some refactoring of the Worklogs controller class.

--- app/Http/Controllers/WorklogController.php
+++ app/Http/Controllers/WorklogController.php
@@ -21,7 +21,7 @@ class WorklogController extends Controller
 	}
 
     public function startService() {
-
+		// check if the unauthorized user is trying to initiate this service (taking the next-in-line customer for a service)
 		 if (! Auth::check()) {
 			 flash( 'Please login to your account.')->warning()->important();
 			 return view('login');
@@ -35,20 +35,22 @@ class WorklogController extends Controller
 			 return view('barbers.create');
 		 }
 
-		 // check if there is any customers in the waitlist
+		 // check if there is any customers on the waitlist
 		 if (Walkin::all()->isEmpty()) {
-			 flash('Hello ' .$user->name .'! There is no customers in the waitlist at the moment.'
+			 flash('Hello ' .$user->name .'! There is no customers on the waiting list at the moment.'
 			 	.' Please check back later.')->warning()->important();
 			 return view('barbers');
 		 }
 
-		 // get the next in line customer from the 'walkins' table
+		 // get the next-in-line customer from the 'walkins' table
 		 $nextCustomer = Walkin::oldest('updated_at')->first();
 
-		 // check if this employee is already in the current worklogs table
-		 $currBarber = Worklog::find($barber->id);
-
-		 $record = ($currBarber) ? $currBarber: new Worklog();
+		 // check if the current barber already has a record in the today's worklog
+		 if (Worklog::find($barber->id)) {
+			 $record = Worklog::find($barber->id)->first();
+		 } else {
+			 $record = new Worklog();
+		 }
 
 		 // get all the required data from the walkins table
 		 $record->barber_id = $barber->id;
@@ -60,7 +62,7 @@ class WorklogController extends Controller
 		 // create a flash message
 		 $flash_message = 'Hi ' .$barber->name .'!'
 			 .' Your next customer, ' .$nextCustomer->customer_name .', needs ' .$nextCustomer->service
-			 .' Tne approximate service time is ' .$record->service_time .' minutes';
+			 .' The approximate service time is ' .$record->service_time .' minutes';
 
 
 		 // delete the first-in-line customer from the queue (walkins table)
