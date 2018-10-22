4c5797cddd3a37a2c81b0025a27595e99124879e
Asya Nikitina <anikitina@cnm.edu>
2018-10-21 20:44:36 -0600

    Finish writing the business logic for the Worklogs controller.

--- app/Http/Controllers/WorklogController.php
+++ app/Http/Controllers/WorklogController.php
@@ -35,52 +35,38 @@ class WorklogController extends Controller
 			 return view('barbers.create');
 		 }
 
-
-		 /*$barber = Barber::where('user_id', '=', $user->id)->first();*/
-		 /*$barber = Barber::first(['user_id' => $user->id]);*/
-
-		/* if (!$barber) {
-			 flash('Hello ' .$user->name .'! Please create your account.')->warning()->important();
-			 return view('barbers.create');
-		 }*/
-
-		 /*if (Auth::check()) {
-			 $barber= new Barber($request->all());
-			 $barber->user_id = Auth::user()->id;*/
-
-		 $barber = Barber::find(Auth::user());
-		 $barberId = $barber->id;
-
 		 // check if there is any customers in the waitlist
 		 if (Walkin::all()->isEmpty()) {
-		 	$flash_message = 'The waitlist is currently empty.';
+			 flash('Hello ' .$user->name .'! There is no customers in the waitlist at the moment.'
+			 	.' Please check back later.')->warning()->important();
+			 return view('barbers');
 		 }
 
 		 // get the next in line customer from the 'walkins' table
-		 $nextCustomer = Walkin::latest('updated_at')->get();
+		 $nextCustomer = Walkin::oldest('updated_at')->first();
 
 		 // check if this employee is already in the current worklogs table
-		 $record = Worklog::find($barberId)->count() ? Worklog::find($barberId) : new Worklog();
+		 $currBarber = Worklog::find($barber->id);
+
+		 $record = ($currBarber) ? $currBarber: new Worklog();
 
 		 // get all the required data from the walkins table
-		 $record->barber_id = $barberId;
+		 $record->barber_id = $barber->id;
 		 $record->ticket_id = $nextCustomer->id;
 		 $record->service_time = $nextCustomer->service_time;
 
 		 $record->save();
 
-		 $customerName = $nextCustomer->customer_name;
-		 $barberName = $barber->name;
-
 		 // create a flash message
-		 $flash_message = 'Hi ' .$barber->name .'!\n'
-			 .'Your next customer, ' .$nextCustomer->customer_name .', needs ' .$nextCustomer->service .'\n'
-			 .'Tne approximate service time is ' .$record->service_time .' minutes';
+		 $flash_message = 'Hi ' .$barber->name .'!'
+			 .' Your next customer, ' .$nextCustomer->customer_name .', needs ' .$nextCustomer->service
+			 .' Tne approximate service time is ' .$record->service_time .' minutes';
 
-		 //session()->flash('message', $flash_message);
 
 		 // delete the first-in-line customer from the queue (walkins table)
 		 $nextCustomer->delete();
+
+		 flash($flash_message)->success()->important();;
 
 		 return view('worklog.index');
 	 }
